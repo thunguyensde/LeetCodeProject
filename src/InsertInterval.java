7800f3923626d471a614911497f7f9a28caf621c
thunguyenus99 <thu.nguyen.us99@gmail.com>
2024-01-22 23:50:24 -0800

    [57.Insert Interval] Solved (un-enhanced)

---
+++ src/InsertInterval.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntFunction;
+
+public class InsertInterval {
+    /*
+    57.
+
+    requirement:
+    You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi]
+    represent the start and the end of the ith interval and intervals is sorted in ascending order by starti.
+    You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+    Insert newInterval into intervals such that intervals is still sorted in ascending order by starti
+    and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+    Return intervals after the insertion.
+
+    test case:
+    [0,2][4,6][8,9]
+
+    [3,3] -> [0,2][3,3][4,6][8,9]
+    [1,3] -> [0,3][4,6][8,9]
+    [3,5] -> [0,2][3,6][8,9]
+    [1,5] -> [0,6][8,9]
+
+    solution:
+    binary search
+    find an interval where it includes newInterval[0], if can't find, return right
+    find an interval where it includes newInterval[1], if can't find, return left
+
+    dry run:
+
+    complexity:
+    - time: O(n)
+    - space: O(n)
+    */
+
+    public int[][] insertInterval(int[][] intervals, int[] newInterval) {
+        int n = intervals.length;
+
+        if (n == 0) {
+            return new int[][]{newInterval};
+        }
+
+        if (newInterval[0] > intervals[n - 1][1]) {
+            int[][] insertedArray = new int[n + 1][];
+            for (int i = 0; i < n; i++) {
+                insertedArray[i] = intervals[i];
+            }
+            insertedArray[n] = newInterval;
+            return insertedArray;
+        }
+
+        if (newInterval[1] < intervals[0][0]) {
+            int[][] insertedArray = new int[n + 1][];
+            insertedArray[0] = newInterval;
+            for (int i = 1; i <= n; i++) {
+                insertedArray[i] = intervals[i - 1];
+            }
+            return insertedArray;
+        }
+
+        int lowerBoundIntervalIndex = binarySearchOnIntervals(intervals, newInterval[0], true); // 1
+        int upperBoundIntervalIndex = binarySearchOnIntervals(intervals, newInterval[1], false); // 0
+
+        List<int[]> intervalList = new ArrayList<>();
+        for (int i = 0; i < lowerBoundIntervalIndex; i++) {
+            intervalList.add(intervals[i]);
+        }
+        intervalList.add(new int[]{
+                Math.min(newInterval[0], intervals[lowerBoundIntervalIndex][0]),
+                Math.max(newInterval[1], intervals[upperBoundIntervalIndex][1])
+        });
+        for (int i = upperBoundIntervalIndex + 1; i < n; i++) {
+            intervalList.add(intervals[i]);
+        }
+
+        return intervalList.toArray(value -> new int[0][]);
+    }
+
+    private int binarySearchOnIntervals(int[][] intervals, int num, boolean returnUpperBoundIfNotFound) {
+        int left = 0;
+        int right = intervals.length - 1;
+        while (left <= right) {
+            int mid = right + (left - right) / 2;
+            if (intervals[mid][0] <= num && intervals[mid][1] >= num) {
+                return mid;
+            }
+            if (intervals[mid][0] > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return returnUpperBoundIfNotFound ? left : right;
+    }
+}
