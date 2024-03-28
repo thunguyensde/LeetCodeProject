e7cb6dc3157ca4333009791c8f09a69f9da5f3b7
thunguyenus99 <thu.nguyen.us99@gmail.com>
2024-03-27 22:52:36 -0700

    [448.Find All Numbers Disappeared In An Array] Solved

---
+++ src/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.function.ToIntFunction;
+
+public class FindAllNumbersDisappearedInAnArray {
+    /*
+    448.
+
+    requirement:
+    Given an array nums of n integers where nums[i] is in the range [1, n],
+    return an array of all the integers in the range [1, n] that do not appear in nums.
+
+    test case:
+    1 1 1 1 1
+    1 2 2 3 4
+
+    */
+
+    public List<Integer> findDisappearedNumber(int[] nums) {
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (!set.contains(i)) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
