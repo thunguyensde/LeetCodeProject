8ecc7ce8b13ea8fde2e3d6fac2dd1c45f3f805f8
thunguyenus99 <thu.nguyen.us99@gmail.com>
2024-04-16 02:33:48 -0700

    [53.Maximum Subarray] Kadane's Algorithm

--- src/MaximumSubarray.java
+++ src/MaximumSubarray.java
@@ -34,4 +34,18 @@ public class MaximumSubarray {
         }
         return largestSum;
     }
+
+    public int kadaneAlgorithm(int[] nums) {
+        int n = nums.length;
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            currentSum += nums[i];
+            maxSum = Math.max(maxSum, currentSum);
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
 }
