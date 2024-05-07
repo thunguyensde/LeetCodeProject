58ab12d42e5fd5304fbbb82126f8d5038f046023
thunguyenus99 <thu.nguyen.us99@gmail.com>
2024-05-06 23:25:59 -0500

    [268.Missing Number] Solved

---
+++ src/MissingNumber.java
@@ -0,0 +1,19 @@
+public class MissingNumber {
+    /*
+    268.
+
+
+    */
+
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        int xor = n;
+        for (int i = 0; i < n; i++) {
+            xor ^= nums[i];
+            xor ^= i;
+        }
+
+        return xor;
+    }
+}
