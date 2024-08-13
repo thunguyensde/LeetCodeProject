c4a183d484a8508de38be30b77bafac43398b306
thunguyenus99 <thu.nguyen.us99@gmail.com>
2024-08-13 10:26:39 -0700

    [97. Interleaving String] Solved

---
+++ src/InterleavingString.java
@@ -0,0 +1,63 @@
+public class InterleavingString {
+    /*
+    97.
+
+    */
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        // abcde
+        // 12345
+
+        // ab c de
+        // 123 45
+        // ab 123 c 45 de
+
+        // ab cde
+        // 1 2 345
+        // 1 ab 2 cde 345
+
+        // ab cde
+        // 1234 5
+        // ab 1234 cde 5
+        // 1234 ab 5 cde
+
+        // ab | cde
+        // 12 | 345
+
+        // 0123456789
+        // ab12cde345
+
+
+        // dp[i][j]: s1[:i-1] and s2[:j-1] interleave to make s3[:i+j-1]
+        // dp[0][0]: true
+        // dp[i][0] = dp[i - 1][0] && s1[i - 1]==s3[i - 1]
+        // dp[0][j] = dp[0][j - 1] && s2[j - 1]==s3[j - 1]
+        // dp[i][j] = (dp[i - 1][j] && s1[i - 1]==s3[i + j - 1]) ||
+        //            (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1])
+
+        int m = s1.length(), n = s2.length();
+
+        if (m + n != s3.length()) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+        }
+
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) ||
+                        (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+
+        return dp[m][n];
+    }
+}
